aab78c6ce3b1e1e8d5169fb1c27efa91df00f122
Alina-kio <alinaolkim@gmail.com>
2023-08-24 01:44:17 +0600

    Postman

--- accounts/views.py
+++ accounts/views.py
@@ -9,7 +9,7 @@ from django.utils import timezone
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from django.contrib.auth import authenticate
-from .models import Profile
+from .models import *
 
 
 class RegistrationView(APIView):
@@ -123,3 +123,20 @@ class ProfileView(APIView):
                              "email": user.email,}, 
                              status=status.HTTP_200_OK)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+
+
+
+
+# view for superuser token, need to check in Postman
+
+from rest_framework_simplejwt.tokens import RefreshToken
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+user = User.objects.get(username='alinakim')  # Замените 'your_username' на имя суперпользователя
+refresh = RefreshToken.for_user(user)
+
+access_token = str(refresh.access_token)
+refresh_token = str(refresh)
+print(access_token)
